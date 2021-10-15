7cb70b0709de58c9841d270569481273600d8c93
konriz <konriz@gmail.com>
2021-10-15 22:09:52 +0200

    HTML

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function determinePath(request) {
   const {url} = request;
   const urlArray = url.split("/");
-  return urlArray.length > 3 ? urlArray.slice(3).join("/") : "" ;
+  return urlArray.length > 3 ? urlArray.slice(3).join("/") : "";
 }
 
 function handleGet(path) {
-  return new Response("Hello world, you are on " + path, {
+  return new Response(getHtml(path), {
     status: 200,
-    headers: {"content-type": "text/plain"},
+    headers: {"content-type": "text/html"},
   });
+}
+
+function getHtml(path) {
+  return `
+<div style="display: flex; justify-content: center; align-items: center;">
+    <h1>Welcome to ${path}</h1>
+    <p>Nothing to see here</p>
+  </div>
+`
 }
 
 addEventListener("fetch", (event) => {
