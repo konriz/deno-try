e266591050329a16dbddc9c0372b5fb2a6583b3f
konriz <konriz@gmail.com>
2021-10-15 21:56:40 +0200

    Use with deno deploy

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+function determinePath(request) {
+  const {url} = request;
+  const urlArray = url.split("/");
+  return urlArray.length > 3 ? urlArray.slice(3).join("/") : "" ;
+}
+
+function handleGet(path) {
+  return new Response("Hello world, you are on " + path, {
+    status: 200,
+    headers: {"content-type": "text/plain"},
+  });
+}
+
 addEventListener("fetch", (event) => {
+  const {request} = event;
+  const path = determinePath(request);
+  if (request.method === "GET") {
+    return event.respondWith(handleGet(path));
+  }
   event.respondWith(
-    new Response("Hello world", {
-      status: 200,
-      headers: { "content-type": "text/plain" },
-    }),
-  );
+    new Response("Get failed", {status: 500, headers: {"content-type": "text/plain"}})
+  )
 });
